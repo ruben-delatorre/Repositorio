88f05237941e6cec27cfa783404c1a77eef51535
ruben-delatorre <72303296+ruben-delatorre@users.noreply.github.com>
2020-10-21 09:41:26 +0200

    Add files via upload

---
+++ ejercicio8bucles.java
@@ -0,0 +1,9 @@
+public class ejercicio8bucles {
+public static void main(String[] args) {
+System.out.print("Introduzca un número entero y le mostraré la tabla de multiplicar: ");
+int numeroIntroducido = Integer.parseInt(System.console().readLine());
+for (int i = 0; i <= 10; i++) {
+System.out.println(numeroIntroducido + " x " + i + " = " + numeroIntroducido * i);
+}
+}
+}
