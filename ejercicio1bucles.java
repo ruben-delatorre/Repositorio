7651e2846ea172b987c76b62eb9b90c85ad88783
ruben-delatorre <72303296+ruben-delatorre@users.noreply.github.com>
2020-10-21 09:40:34 +0200

    Add files via upload

---
+++ ejercicio1bucles.java
@@ -0,0 +1,8 @@
+public class ejercicio1bucles {
+    public static void main(String[] args) { 
+      for (int i = 0; i <= 100; i = i + 5) {
+        System.out.println(i);
+        }
+      }
+    }
+    
