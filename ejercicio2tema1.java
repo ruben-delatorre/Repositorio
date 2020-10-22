722583cdc5ac2f2aeea576916de488769ed9214a
ruben-delatorre <72303296+ruben-delatorre@users.noreply.github.com>
2020-10-22 11:01:05 +0200

    Delete ejercicio2tema1.java

--- ejercicio2tema1.java
+++
@@ -1,7 +0,0 @@
-public class ejercicio2tema1 {
-  public static void main(String[] args) {
-      System.out.println("Rubén de la Torre López");
-      System.out.println("c/Paul Valery");
-      System.out.println("Tlfn: 695 90 22 27");
-  }
-}
