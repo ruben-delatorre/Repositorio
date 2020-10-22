378963b43d5638f401114d05e3f7b823203f74ae
ruben-delatorre <72303296+ruben-delatorre@users.noreply.github.com>
2020-10-22 14:29:30 +0200

    Delete ejercicio7variables.java

--- Tema2. Variables/ejercicio7variables.java
+++
@@ -1,8 +0,0 @@
-public class ejercicio7variables {
-public static void main(String[] args) {
-String palabra1 = "salta";
-String palabra2 = "monte";
-char ultimaLetra = 's';
-System.out.println(palabra1 + palabra2 + ultimaLetra);
-}
-}
