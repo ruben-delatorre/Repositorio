4a2c3dbe128726abe41bab79be475bede3f54322
ruben-delatorre <72303296+ruben-delatorre@users.noreply.github.com>
2020-10-22 14:31:35 +0200

    Delete ejercicio3tema3.java

--- Tema3. Lectura de datos desde el teclado/ejercicio3tema3.java
+++
@@ -1,8 +0,0 @@
-public class ejercicio3tema3 {
-public static void main(String[] args) {
-System.out.print("Introduce la cantidad de pesetas: ");
-int pesetas = Integer.parseInt(System.console().readLine());
-double euros = (pesetas / 166.386);
-System.out.print(pesetas + " pesetas son " + euros + " euros. ");
-}
-}
