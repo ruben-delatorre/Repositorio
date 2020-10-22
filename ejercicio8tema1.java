a2bdf64d6f8dacc85a25517b86779f9f97af73b6
ruben-delatorre <72303296+ruben-delatorre@users.noreply.github.com>
2020-10-22 11:04:17 +0200

    Delete ejercicio8tema1.java

--- ejercicio8tema1.java
+++
@@ -1,9 +0,0 @@
-public class ejercicio8tema1 {
-public static void main(String[] args) {
-System.out.println("*********");
-System.out.println(" *     *");
-System.out.println("  *   *");
-System.out.println("   * *");
-System.out.println("    *");
-}
-}
