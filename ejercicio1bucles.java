da20263b5dffae7b386c3c3660393e6242929a81
ruben-delatorre <72303296+ruben-delatorre@users.noreply.github.com>
2020-10-22 11:00:33 +0200

    Delete ejercicio1bucles.java

--- ejercicio1bucles.java
+++
@@ -1,8 +0,0 @@
-public class ejercicio1bucles {
-    public static void main(String[] args) { 
-      for (int i = 0; i <= 100; i = i + 5) {
-        System.out.println(i);
-        }
-      }
-    }
-    
