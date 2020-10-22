67b954ae698730a0971e23aad3ca42c206c0ae70
ruben-delatorre <72303296+ruben-delatorre@users.noreply.github.com>
2020-10-22 11:02:57 +0200

    Delete ejercicio5variables.java

--- ejercicio5variables.java
+++
@@ -1,7 +0,0 @@
-public class ejercicio5variables {
-public static void main(String[] args) {
-int pesetas = 2000;
-double euros = (pesetas / 166.386);
-System.out.println(pesetas + " pesetas son " + euros + " euros.");
-}
-}
