4be0302d64c72c29ee7ae694ac8c076e50864a3b
ruben-delatorre <72303296+ruben-delatorre@users.noreply.github.com>
2020-10-22 10:59:56 +0200

    Delete ejercicio11bucles.java

--- ejercicio11bucles.java
+++
@@ -1,9 +0,0 @@
-public class ejercicio11bucles {
-public static void main(String[] args) {
-System.out.print("Introduzca un número: ");
-int numeroIntroducido = Integer.parseInt(System.console().readLine());
-for (int i = numeroIntroducido; i < numeroIntroducido + 5; i++) {
-System.out.printf("%4d %6d %8d\n", i, i * i, i * i * i);
-}
-}
-}
